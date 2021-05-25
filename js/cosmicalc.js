374af8366b92d602c8cfe5d240f3f53f937543f9
pamapamanent <pamapamanent@gmail.com>
2021-05-25 23:46:34 +0900

    変更　内臓強化値反映、兵科ミス修正

--- js/cosmicalc.js
+++ js/cosmicalc.js
@@ -20,8 +20,8 @@
 (function($, undefined){
 
 /* CONST VARS */
-var VERSION = "4.0.1",
-		LAST_MODIFIED = "2021.05.23",
+var VERSION = "4.0.2",
+		LAST_MODIFIED = "2021.05.25",
 
 	WEB_ACCESS_QUERY = "?mode=getData&target=",
 
